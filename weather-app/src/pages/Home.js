8d32c27c663578450686b32be5693d83aa3c9cab
Annacletter <anna.cletter12@gmail.com>
2024-09-22 10:46:19 -0700

    adjusting the Home and App files

--- weather-app/src/pages/Home.js
+++ weather-app/src/pages/Home.js
@@ -2,43 +2,44 @@ import React, { useState } from 'react';
 import WeatherForm from '../components/WeatherForm';
 import WeatherDisplay from '../components/WeatherDisplay';
 import mockData from '../mockData.json'; // Import your mock JSON data
-import { WiDaySunny, WiRain, WiSnow } from 'react-icons/wi';
+import { WiDaySunny, WiRain, WiSnow } from 'react-icons/wi'; // Weather icons
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../style.css'; // Import your CSS file
-import sunnyImage from '../assets/sunny.jpg';
-import rainyImage from '../assets/rainy.jpg';
-import snowyImage from '../assets/snowy.jpg';
+import './style.css';
 
 const Home = () => {
   const [weather, setWeather] = useState(null);
   const [bgClass, setBgClass] = useState('');
+  const [weatherIcon, setWeatherIcon] = useState(null); // To store the selected icon
 
   const handleSearch = (city) => {
-    // Using the imported mock data
     const mockedWeather = mockData[city] || {
       city: city,
-      temperature: Math.floor(Math.random() * 30) + 15, // Default mock temperature
-      description: 'Clear', // Default description
-      humidity: 50, // Default humidity
-      wind_speed: 10, // Default wind speed
-      main: 'Clear', // Default weather condition
+      temperature: Math.floor(Math.random() * 30) + 15,
+      description: 'Clear',
+      humidity: 50, 
+      wind_speed: 10,
+      main: 'Clear',
     };
 
     setWeather(mockedWeather);
 
-    // Set background class based on the weather condition
+    // Set background class and weather icon based on the weather condition
     switch (mockedWeather.main) {
       case 'Clear':
         setBgClass('sunny');
+        setWeatherIcon(<WiDaySunny size={50} />); // Set the sunny icon
         break;
       case 'Rain':
         setBgClass('rainy');
+        setWeatherIcon(<WiRain size={50} />); // Set the rain icon
         break;
       case 'Snow':
         setBgClass('snowy');
+        setWeatherIcon(<WiSnow size={50} />); // Set the snow icon
         break;
-      default:
+      default: 
         setBgClass('');
+        setWeatherIcon(null); // Reset the icon if the weather condition doesn't match
     }
   };
 
@@ -46,6 +47,7 @@ const Home = () => {
     <div className={`weather-app ${bgClass}`}>
       <WeatherForm onSearch={handleSearch} />
       <WeatherDisplay weather={weather} />
+      {weatherIcon && <div className="weather-icon">{weatherIcon}</div>} {/* Display the weather icon */}
       <style>{`
         .weather-app {
           min-height: 100vh;
@@ -56,16 +58,20 @@ const Home = () => {
           transition: background 0.5s ease;
         }
 
-        .sunny {
-          background-image: url('path/to/sunny.jpg');
+        .sunny { 
+          background-image: url('https://raw.githubusercontent.com/Annacletter/Weather-app/master/weather-app/src/assets/sunny.jpg');
         }
 
         .rainy {
-          background-image: url('path/to/rainy.jpg');
+          background-image: url('https://raw.githubusercontent.com/Annacletter/Weather-app/master/weather-app/src/assets/rainy.jpg');
         }
 
         .snowy {
-          background-image: url('path/to/snowy.jpg');
+          background-image: url('https://raw.githubusercontent.com/Annacletter/Weather-app/master/weather-app/src/assets/snowy.jpg');
+        }
+      
+       .weather-icon {
+          margin-top: 20px;
         }
       `}</style>
     </div>
@@ -73,3 +79,4 @@ const Home = () => {
 };
 
 export default Home;
+

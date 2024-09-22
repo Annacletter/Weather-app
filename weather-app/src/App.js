be0a880b4a9e9b723dba91d6119d1e5596343000
Annacletter <anna.cletter12@gmail.com>
2024-09-22 10:19:22 -0700

    adjusting the Home and App files

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 // Import other pages/components if you plan to add more routes
 
 function App() {
   return (
     <Router>
-      <Switch>
+      <Routes>
         <Route exact path="/" component={Home} />
         {/* Add other routes as needed */}
-      </Switch>
+      </Routes>
     </Router>
   );
 }

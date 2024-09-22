235e3805113be9c50348964f12e61530b18841c2
Annacletter <anna.cletter12@gmail.com>
2024-09-22 05:43:54 -0700

    Updated App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Home from './pages/Home';
+import Weather form from './components/WeatheForm';
+import Weather display from './components/WeatherDisplay';
 
 function App() {
   return (

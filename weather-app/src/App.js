c3f73d24af33790045505106434eb1890270ab04
Annacletter <anna.cletter12@gmail.com>
2024-09-26 12:03:57 -0700

    added landing page to App file

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/Home';          // Import Home page
+import LandingPage from './components/LandingPage';  // Import Landing page
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* Landing page at the root */}
+        <Route path="/" element={<LandingPage />} />
+
+        {/* Home page accessible at '/home' */}
+        <Route path="/home" element={<Home />} />
+
+        {/* Add more routes here if needed */}
       </Routes>
     </Router>
   );

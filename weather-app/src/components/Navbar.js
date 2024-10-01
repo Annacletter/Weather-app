38e42ffd19c94b100d7ce3dd3599c8b242ecf909
Annacletter <anna.cletter12@gmail.com>
2024-10-01 12:45:17 -0700

    fixing the styling file

--- weather-app/src/components/Navbar.js
+++ weather-app/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "../styles.css";
+import '../styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 

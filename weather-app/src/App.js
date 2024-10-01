55e35d544ffa5ba845eacc8369a7b7aee5f56431
Annacletter <anna.cletter12@gmail.com>
2024-10-01 12:01:03 -0700

    adding routes

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -3,14 +3,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';          // Import the Home page component
 import LandingPage from './components/LandingPage';  // Import the LandingPage component
+import Contact from './components/Contact';  // Assuming you have a Contact component
+import Navbar from './components/Navbar';  // Assuming you have a Navbar component
 
 const App = () => (
   <Router>
     <Navbar />
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Home" element={<LandingPage>} />
-      <Route path="/Check weather status" element={<Home />} />
+      <Route path="/" element={<LandingPage />} />  {/* Fixing the missing closing tag */}
+      <Route path="/Home" element={<Home />} />
+      <Route path="/Check-weather-status" element={<Home />} />  {/* Fixed the route path name */}
       <Route path="/contact" element={<Contact />} />
     </Routes>
   </Router>

62ec80d02d121308b0353e58a3000687b905e263
Annacletter <anna.cletter12@gmail.com>
2024-10-01 11:48:24 -0700

    adding routes

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -4,20 +4,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';          // Import the Home page component
 import LandingPage from './components/LandingPage';  // Import the LandingPage component
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* Landing page at the root */}
-        <Route path="/" element={<LandingPage />} />
-
-        {/* Home page accessible at '/home' */}
-        <Route path="/home" element={<Home />} />
-
-        {/* Add more routes as needed */}
-      </Routes>
-    </Router>
-  );
-}
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Home" element={<LandingPage>} />
+      <Route path="/Check weather status" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </Router>
+);
 
 export default App;

37e2e18ccb9ad57d10f18c782715b02f3099d68a
Annacletter <anna.cletter12@gmail.com>
2024-10-06 11:30:05 -0700

    amended the style file

--- weather-app/src/components/Navbar.js
+++ weather-app/src/components/Navbar.js
@@ -9,11 +9,19 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
                 <a className="navbar-brand" href="/">Weather Application</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button 
+                    className="navbar-toggler" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#navbarNav" 
+                    aria-controls="navbarNav" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation"
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ml-auto">
+                    <ul className="navbar-nav ms-auto">  {/* Bootstrap 5 uses ms-auto instead of ml-auto */}
                         <li className="nav-item">
                             <Link className="nav-link" to="/">Home</Link>
                         </li>
@@ -34,4 +42,3 @@ const Navbar = () => {
 }
 
 export default Navbar;
-
